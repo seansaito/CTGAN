3e1cad884ee1dbb491b9d01c23606a0275f8bfb1
i330688 <sean.saito@sap.com>
2020-06-08 15:46:00 +0800

    Added loss logging after training

--- ctgan/synthesizer.py
+++ ctgan/synthesizer.py
@@ -238,10 +238,10 @@ class CTGANSynthesizer(object):
                 loss_g.backward()
                 optimizerG.step()
 
-            if self.verbose:
-                print("Epoch %d, Loss G: %.4f, Loss D: %.4f" %
-                      (i + 1, loss_g.detach().cpu(), loss_d.detach().cpu()),
-                      flush=True)
+        if self.verbose:
+            print("Epoch %d, Loss G: %.4f, Loss D: %.4f" %
+                  (i + 1, loss_g.detach().cpu(), loss_d.detach().cpu()),
+                  flush=True)
 
     def sample(self, n):
         """Sample data similar to the training data.
